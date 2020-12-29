96ec3cb16060ab823c2de61fc2dbed6d468c9ae0
dmvishwas <37170852+dmvishwas@users.noreply.github.com>
2020-12-29 22:33:42 +0530

    SpringBoot CRUD, JPA

---
+++ CRUD/src/main/java/com/crud/workset/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.crud.workset.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.crud.workset.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+	Product findByName(String name);
+	
+}
